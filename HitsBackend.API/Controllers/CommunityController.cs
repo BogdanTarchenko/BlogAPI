e4c0f0b5f03f98ca5836a61b20faef3e7f098c5c
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-12-04 00:10:36 +0700

    fix CommunityController endpoints sequence

--- HitsBackend.API/Controllers/CommunityController.cs
+++ HitsBackend.API/Controllers/CommunityController.cs
@@ -29,6 +29,102 @@ public class CommunityController : ControllerBase
     }
     
     /// <summary>
+    /// Get user's community list (with the greatest user's role in the community)
+    /// </summary>
+    [HttpGet("my")]
+    [Authorize]
+    public async Task<ActionResult<List<CommunityUserDto>>> GetUserCommunities()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+
+        var communities = await _communityService.GetUserCommunitiesAsync(Guid.Parse(userId));
+        return Ok(communities);
+    }
+    
+    /// <summary>
+    /// Get information about community
+    /// </summary>
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CommunityFullDto>> GetCommunityById(Guid id)
+    {
+        var community = await _communityService.GetCommunityByIdAsync(id);
+        if (community == null)
+        {
+            return NotFound();
+        }
+        return Ok(community);
+    }
+    
+    /// <summary>
+    /// Create a post in the specified community
+    /// </summary>
+    [HttpPost("{id}/post")]
+    [Authorize]
+    public async Task<IActionResult> CreatePostInCommunity(Guid id, [FromBody] CreatePostDto dto)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+        
+        var postId = await _communityService.CreatePostInCommunityAsync(id, Guid.Parse(userId), dto);
+        return Ok(postId);
+    }
+    
+    /// <summary>
+    /// Get community's posts
+    /// </summary>
+    [HttpGet("{id}/post")]
+    [Authorize]
+    [AllowAnonymous]
+    public async Task<ActionResult<PostPagedListDto>> GetPostsByCommunityId(
+        Guid id,
+        [FromQuery] List<Guid>? tags,
+        [FromQuery] PostSorting sorting = PostSorting.CreateDesc,
+        [FromQuery] int page = 1,
+        [FromQuery] int size = 5)
+    {
+        Guid? userId = null;
+        var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!string.IsNullOrEmpty(userIdClaim))
+        {
+            userId = Guid.Parse(userIdClaim);
+        }
+
+        var posts = await _communityService.GetPostsByCommunityIdAsync(id, tags, sorting, page, size, userId);
+        return Ok(posts);
+    }
+    
+    /// <summary>
+    /// Get the greatest user's role in the community (or null if the user is not a member of the community)
+    /// </summary>
+    [HttpGet("{id}/role")]
+    [Authorize]
+    public async Task<IActionResult> GetUserRoleInCommunity(Guid id)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+
+        var role = await _communityService.GetUserRoleInCommunityAsync(id, Guid.Parse(userId));
+
+        if (role == null)
+        {
+            return Ok(new { Role = role} );
+        }
+
+        return Ok(new { Role = role.ToString() });
+    }
+    
+    /// <summary>
     /// Subscribe a user to the community
     /// </summary>
     [HttpPost("{id}/subscribe")]
@@ -60,101 +156,5 @@ public class CommunityController : ControllerBase
 
         await _communityService.UnsubscribeAsync(id, Guid.Parse(userId));
         return Ok();
-    }
-
-    /// <summary>
-    /// Get user's community list (with the greatest user's role in the community)
-    /// </summary>
-    [HttpGet("my")]
-    [Authorize]
-    public async Task<ActionResult<List<CommunityUserDto>>> GetUserCommunities()
-    {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
-            return Unauthorized();
-        }
-
-        var communities = await _communityService.GetUserCommunitiesAsync(Guid.Parse(userId));
-        return Ok(communities);
-    }
-
-    /// <summary>
-    /// Get information about community
-    /// </summary>
-    [HttpGet("{id}")]
-    public async Task<ActionResult<CommunityFullDto>> GetCommunityById(Guid id)
-    {
-        var community = await _communityService.GetCommunityByIdAsync(id);
-        if (community == null)
-        {
-            return NotFound();
-        }
-        return Ok(community);
-    }
-
-    /// <summary>
-    /// Create a post in the specified community
-    /// </summary>
-    [HttpPost("{id}/post")]
-    [Authorize]
-    public async Task<IActionResult> CreatePostInCommunity(Guid id, [FromBody] CreatePostDto dto)
-    {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
-            return Unauthorized();
-        }
-        
-        var postId = await _communityService.CreatePostInCommunityAsync(id, Guid.Parse(userId), dto);
-        return Ok(postId);
-    }
-
-    /// <summary>
-    /// Get the greatest user's role in the community (or null if the user is not a member of the community)
-    /// </summary>
-    [HttpGet("{id}/role")]
-    [Authorize]
-    public async Task<IActionResult> GetUserRoleInCommunity(Guid id)
-    {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
-            return Unauthorized();
-        }
-
-        var role = await _communityService.GetUserRoleInCommunityAsync(id, Guid.Parse(userId));
-
-        if (role == null)
-        {
-            return Ok(new { Role = role} );
-        }
-
-        return Ok(new { Role = role.ToString() });
-    }
-
-    /// <summary>
-    /// Get community's posts
-    /// </summary>
-    [HttpGet("{id}/post")]
-    [Authorize]
-    [AllowAnonymous]
-    public async Task<ActionResult<PostPagedListDto>> GetPostsByCommunityId(
-        Guid id,
-        [FromQuery] List<Guid>? tags,
-        [FromQuery] PostSorting sorting = PostSorting.CreateDesc,
-        [FromQuery] int page = 1,
-        [FromQuery] int size = 5)
-    {
-        Guid? userId = null;
-        var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-        if (!string.IsNullOrEmpty(userIdClaim))
-        {
-            userId = Guid.Parse(userIdClaim);
-        }
-
-        var posts = await _communityService.GetPostsByCommunityIdAsync(id, tags, sorting, page, size, userId);
-        return Ok(posts);
     }
 } 
