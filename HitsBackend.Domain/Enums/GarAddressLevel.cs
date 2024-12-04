099458b0bee7c42f6db7cf4285a0ba1b7c17a672
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-12-05 00:31:53 +0700

    add GarAddressLevel enum

---
+++ HitsBackend.Domain/Enums/GarAddressLevel.cs
@@ -0,0 +1,22 @@
+namespace HitsBackend.Domain.Enums;
+
+public enum GarAddressLevel
+{
+    Region,
+    AdministrativeArea,
+    MunicipalArea,
+    RuralUrbanSettlement,
+    City,
+    Locality,
+    ElementOfPlanningStructure,
+    ElementOfRoadNetwork,
+    Land,
+    Building,
+    Room,
+    RoomInRooms,
+    AutonomousRegionLevel,
+    IntracityLevel,
+    AdditionalTerritoriesLevel,
+    LevelOfObjectsInAdditionalTerritories,
+    CarPlace
+}
